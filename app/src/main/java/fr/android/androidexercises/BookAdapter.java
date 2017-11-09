a9d331a94da8dd18007c263fd0dbf45038a3af9a
Clément Garbay <clementgarbay@gmail.com>
2017-11-09 16:18:18 +0100

    Implement ListView with custom view

--- app/src/main/java/fr/android/androidexercises/BookAdapter.java
+++ app/src/main/java/fr/android/androidexercises/BookAdapter.java
@@ -1,39 +1,49 @@
 package fr.android.androidexercises;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 import java.util.List;
 
+import static java.util.Objects.isNull;
+
 public class BookAdapter extends BaseAdapter {
 
     private final List<Book> books;
+    private final LayoutInflater inflater;
 
     public BookAdapter(Context context, List<Book> books) {
         this.books = books;
-        // TODO LayoutInflater.from()
+        this.inflater = LayoutInflater.from(context);
     }
 
     @Override
     public int getCount() {
-        return 0; // TODO
+        return books.size();
     }
 
     @Override
     public Book getItem(int position) {
-        return null; // TODO
+        return books.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0; // TODO
+        return books.get(position).hashCode();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null; // TODO
+        if (isNull(convertView)) {
+            convertView = inflater.inflate(R.layout.custom_view_item_book, parent, false);
+        }
+
+        ((BookItemView) convertView).bindView(getItem(position));
+
+        return convertView;
     }
 
 }
--- app/src/main/java/fr/android/androidexercises/BookItemView.java
+++ app/src/main/java/fr/android/androidexercises/BookItemView.java
@@ -2,9 +2,14 @@ package fr.android.androidexercises;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class BookItemView extends LinearLayout {
+
+    private TextView nameTextView;
+    private TextView priceTextView;
 
     public BookItemView(Context context) {
         this(context, null);
@@ -21,10 +26,13 @@ public class BookItemView extends LinearLayout {
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        // TODO findViewById()
+
+        nameTextView = findViewById(R.id.nameTextView);
+        priceTextView = findViewById(R.id.priceTextView);
     }
 
     public void bindView(Book book) {
-        // TODO setText()
+        nameTextView.setText(book.name);
+        priceTextView.setText(String.valueOf(book.price));
     }
 }
