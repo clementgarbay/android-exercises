d81c757bf11ce54d427aca761e5fe44952c2e551
Clément Garbay <clementgarbay@gmail.com>
2017-11-09 15:01:08 +0100

    Add a date picker dialog

--- app/src/main/java/fr/android/androidexercises/BookActivity.java
+++ app/src/main/java/fr/android/androidexercises/BookActivity.java
@@ -1,9 +1,17 @@
 package fr.android.androidexercises;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import static fr.android.androidexercises.Constants.BOOK;
 
@@ -22,6 +30,22 @@ public class BookActivity extends AppCompatActivity {
 
         TextView nameTextView = (TextView) findViewById(R.id.nameTextView);
         nameTextView.setText(book.getName());
+
+        Button selectDateBtn = findViewById(R.id.selectDateBtn);
+        selectDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(new Date());
+
+                new DatePickerDialog(BookActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                        Toast.makeText(BookActivity.this, dayOfMonth + "/" + (monthOfYear+1) + "/" + year, Toast.LENGTH_LONG).show();
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 
 }
