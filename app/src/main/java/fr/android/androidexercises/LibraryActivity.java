14958dd293de90dabd5deaa37d657a5fa9610771
Clément Garbay <clementgarbay@gmail.com>
2017-11-09 17:30:26 +0100

    Use Glide to load and view image from URL

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,9 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -10,8 +13,10 @@ public class LibraryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
 
-        // TODO Get image view and load image form URL
-        // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
+        // Get image view and load image from URL
+        Glide.with(this)
+                .load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg")
+                .into((ImageView) findViewById(R.id.downloadImageView));
     }
 
 }
