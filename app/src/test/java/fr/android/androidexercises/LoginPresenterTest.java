6b6eda4738d986eb653d301f7594a36dba391c7e
Clément Garbay <clementgarbay@gmail.com>
2017-11-10 14:17:20 +0100

    Implement basic unit tests

--- app/src/test/java/fr/android/androidexercises/LoginPresenterTest.java
+++ app/src/test/java/fr/android/androidexercises/LoginPresenterTest.java
@@ -1,5 +1,45 @@
 package fr.android.androidexercises;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LoginPresenterTest {
+
+    @Mock LoginActivity activity;
+    @InjectMocks LoginPresenter loginPresenter;
+
+    @Test
+    public void password_should_be_valid() throws Exception {
+        loginPresenter.checkCredentials("password");
+        Mockito.verify(activity).logged();
+        Mockito.verify(activity).message(R.string.text_logged);
+    }
+
+    @Test
+    public void password_should_not_be_valid_with_null() throws Exception {
+        loginPresenter.checkCredentials(null);
+        Mockito.verify(activity).notLogged();
+        Mockito.verify(activity).message(R.string.notLogged);
+    }
+
+    @Test
+    public void password_should_not_be_valid_with_shorter_than_3() throws Exception {
+        loginPresenter.checkCredentials("p");
+        Mockito.verify(activity).notLogged();
+        Mockito.verify(activity).message(R.string.notLogged);
+    }
+
+
+    @Test
+    public void password_should_be_valid_with_3() throws Exception {
+        loginPresenter.checkCredentials("pas");
+        Mockito.verify(activity).logged();
+        Mockito.verify(activity).message(R.string.text_logged);
+    }
 
 }
