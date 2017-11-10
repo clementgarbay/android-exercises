af4b44c0f9e26bc4964bac84f99f36d290fbf05a
Clément Garbay <clementgarbay@gmail.com>
2017-11-10 15:10:38 +0100

    Add tests with robolectric

--- app/src/test/java/fr/android/androidexercises/CustomRobolectricTestRunner.java
+++ app/src/test/java/fr/android/androidexercises/CustomRobolectricTestRunner.java
@@ -1,7 +1,15 @@
-package fr.android.androidexercises;
-
-// TODO remove comments when using Robolectric
-
+//package fr.android.androidexercises;
+//
+//import android.os.Build;
+//
+//import org.junit.runners.model.InitializationError;
+//import org.robolectric.RobolectricTestRunner;
+//import org.robolectric.annotation.Config;
+//import org.robolectric.manifest.AndroidManifest;
+//
+//import java.io.File;
+//import java.util.Properties;
+//
 //public class CustomRobolectricTestRunner extends RobolectricTestRunner {
 //
 //    public static final String PATH_ASSET = "../../../assets/" + BuildConfig.FLAVOR + "/" + BuildConfig.BUILD_TYPE;
--- app/src/test/java/fr/android/androidexercises/JUnitLoginActivityTest.java
+++ app/src/test/java/fr/android/androidexercises/JUnitLoginActivityTest.java
@@ -1,5 +1,34 @@
 package fr.android.androidexercises;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.assertj.android.api.Assertions.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
 public class JUnitLoginActivityTest {
 
+    private LoginActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.setupActivity(LoginActivity.class);
+    }
+
+    @Test
+    public void should_set_logged_state() throws Exception {
+        activity.logged();
+        assertThat(activity.loginLayout).isGone();
+        assertThat(activity.loggedText).isVisible();
+    }
+
+    @Test
+    public void should_not_set_logged_state() throws Exception {
+        activity.notLogged();
+        assertThat(activity.loginLayout).isNotGone();
+        assertThat(activity.loggedText).isNotVisible();
+    }
 }
